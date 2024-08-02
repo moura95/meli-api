6b5a60a19a5f12b79d1970d78d9a315615ace3fc
guilherme-moura95 <junior.moura19@hotmail.com>
2024-08-02 23:02:14 +0100

    feature/crud-tickets - Add seed severities

--- db/migrations/000001_init_schema.up.sql
+++ db/migrations/000001_init_schema.up.sql
@@ -5,14 +5,18 @@ CREATE TABLE severities (
                             id SERIAL PRIMARY KEY,
                             level INT UNIQUE NOT NULL,
                             description VARCHAR(255) NOT NULL
-
-
                         );
+
+INSERT INTO severities (level, description) VALUES
+                                                (1, 'issue high'),
+                                                (2, 'high'),
+                                                (3, 'medium'),
+                                                (4, 'low');
 
 CREATE TABLE tickets (
                          id SERIAL PRIMARY KEY,
                          title VARCHAR(60) NOT NULL,
-                         status VARCHAR(20) NOT NULL,
+                         status VARCHAR(20) DEFAULT 'OPEN' NOT NULL CHECK (status IN ('OPEN', 'IN_PROGRESS','BLOCKED','DONE', 'CLOSED')),
                          description TEXT NOT NULL,
                          severity_id INT NOT NULL ,
                          created_at  TIMESTAMP    NOT NULL DEFAULT NOW(),
