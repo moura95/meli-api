45c4401edaa75585f02c952e60d362112655fad0
guilherme-moura95 <junior.moura19@hotmail.com>
2024-08-04 17:08:45 +0100

    feature/implement-categories - queries category

--- internal/repository/querier.go
+++ internal/repository/querier.go
@@ -6,13 +6,20 @@ package repository
 
 import (
 	"context"
+	"database/sql"
 )
 
 type Querier interface {
+	CreateCategory(ctx context.Context, arg CreateCategoryParams) (Category, error)
 	CreateTicket(ctx context.Context, arg CreateTicketParams) (Ticket, error)
+	DeleteCategory(ctx context.Context, id int32) error
 	DeleteTicket(ctx context.Context, id int32) error
+	GetCategoryById(ctx context.Context, id int32) (Category, error)
 	GetTicketById(ctx context.Context, id int32) (Ticket, error)
+	ListCategories(ctx context.Context) ([]Category, error)
+	ListSubCategories(ctx context.Context, parentID sql.NullInt32) ([]Category, error)
 	ListTickets(ctx context.Context) ([]Ticket, error)
+	UpdateCategory(ctx context.Context, arg UpdateCategoryParams) error
 	UpdateTicket(ctx context.Context, arg UpdateTicketParams) error
 }
 
