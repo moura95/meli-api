f2b5c9ac7f2fef2b30bbb86f21fbda9f5a066a23
guilherme-moura95 <junior.moura19@hotmail.com>
2024-08-04 17:05:15 +0100

    feature/implement-categories - queries category

---
+++ db/queries/category.sql
@@ -0,0 +1,31 @@
+-- name: CreateCategory :one
+INSERT INTO categories (name,parent_id)
+VALUES ($1, $2)
+RETURNING *;
+
+
+-- name: DeleteCategory :exec
+DELETE FROM categories
+WHERE id = $1;
+
+-- name: GetCategoryById :one
+SELECT *
+FROM categories
+WHERE id = $1;
+
+-- name: ListCategories :many
+SELECT id, name, parent_id
+FROM categories;
+
+-- name: ListSubCategories :many
+SELECT id, name, parent_id
+FROM categories
+WHERE  parent_id = $1;
+
+
+
+-- name: UpdateCategory :exec
+UPDATE categories
+SET name = COALESCE(sqlc.narg('name'), name),
+    parent_id  = COALESCE(sqlc.narg('parent_id'), parent_id)
+WHERE id = $1;
